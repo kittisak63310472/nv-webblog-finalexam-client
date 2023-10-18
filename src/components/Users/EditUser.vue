5f49a4e58d71de0f9acafb9dce73dcff7c98eafb
mlpn.me <110648389+kittisak63310472@users.noreply.github.com>
2023-10-18 14:00:38 +0700

    client

--- src/components/Users/EditUser.vue
+++ src/components/Users/EditUser.vue
@@ -3,18 +3,16 @@
     <h1>Edit Users</h1>
     <h1>Edit User</h1>
     <form v-on:submit.prevent="editUser">
-      <p>name: <input type="text" v-model="user.name" /></p>
-      <p>lastname: <input type="text" v-model="user.lastname" /></p>
-      <p>email: <input type="text" v-model="user.email" /></p>
-      <p>password: <input type="text" v-model="user.password" /></p>
+      <p>namebook: <input type="text" v-model="user.name" /></p>
+      <p>type: <input type="text" v-model="user.lastname" /></p>
+      <p>price: <input type="text" v-model="user.price" /></p>
       <p><button type="submit">edit user</button></p>
     </form>
     <hr />
     <div>
       <p>name: {{ user.name }}</p>
-      <p>lastname: {{ user.lastname }}</p>
-      <p>email: {{ user.email }}</p>
-      <p>password: {{ user.password }}</p>
+      <p>lastname: {{ user.lastname }}</p> 
+      <p>price: {{ user.price }}</p>
       <p></p>
     </div>
   </div>
--- src/components/Users/ShowUser.vue
+++ src/components/Users/ShowUser.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Show User</h1>
-    <p>id: {{ user.id }}</p>
+    <p>namebook: {{ user.id }}</p>
     <p>ชื่อ-นามสกุล: {{ user.name }} - {{ user.lastname }}</p>
     <p>email: {{ user.email }}</p>
     <p>password: {{ user.password }}</p>
